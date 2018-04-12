6497883aff169f512a585c261a9e29ebb3df519c
Jessica Joseph <jessjeanjo@gmail.com>
2018-04-12 16:00:30 -0400

    no. 771

---
+++ leetcode/771_jewels_stones.cpp
@@ -0,0 +1,22 @@
+// Created by Jessica Joseph on 4/12/18
+
+int numJewelsInStones(string J, string S){
+	map<char, int> container;
+	int jewelCount = 0;
+
+	for (char element: S) {
+		if (container.find(element) != container.end()) {
+			container[element] += 1;
+		} else {
+			container.insert(make_pair(element, 1));
+		}
+	}
+
+	for (char element: J) {
+		if (container.find(element) != container.end()) {
+			jewelCount += container[element];
+		}
+	}
+
+	return jewelCount;
+}
