b5fb2bdf8d9dbaaeabe52722ff0799ef9ba1d225
Jessica Joseph <jessjeanjo@gmail.com>
2018-04-12 15:09:25 -0400

    start one away, must be updated

---
+++ ctci/01_arrays_strings/one_away.cpp
@@ -0,0 +1,24 @@
+// Created by Jessica Joseph on 4/12/18
+
+#include <iostream>
+#include <map>
+#include <string>
+
+using namespace std;
+
+bool oneAway(string strOne, string strTwo) {
+	map<char, int> container;
+	for (auto element: strOne) {
+		if (container.find(element)!= container.end()){
+			container.insert(make_pair())
+		}
+	}
+
+	for (suto element: strTwo){
+	
+	}
+}
+
+int main() {
+	oneAway("pale", "ple");
+}
