1d69e5da27dafe7bc0c7a1c4cc8c0ad5e68536f1
Jessica Joseph <jessjeanjo@gmail.com>
2019-07-09 19:52:42 -0700

    add Sources code explanation note within the introduction

--- Array.playground/Pages/Introduction.xcplaygroundpage/Contents.swift
+++ Array.playground/Pages/Introduction.xcplaygroundpage/Contents.swift
@@ -1,2 +1,6 @@
-// Array Problems
-// Data Structure and Algorithm Problems as they relate to arrays
+/*
+ARRAYS
+
+Data Structure and Algorithm Problems as they relate to arrays.
+Step by Step Solution explanations can be found within the Problem Page's Source Folder
+ */
